0b219cb192f3f5e6374de255f2129e88ce5f9e90
VT <ireact@gmail.com>
2022-11-16 00:47:12 +0530

    updated in 2022

--- emotion/urls.py
+++ emotion/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 app_name = 'emotion'
 
 urlpatterns = [
-    url(r'^$', views.emotion_analysis, name="emotion_anaylsis"),
-    url(r'^type/$', views.emotion_analysis_type, name="emotion_analysis_type"),
-    url(r'^import/$', views.emotion_analysis_import, name="emotion_analysis_import"),
+    path('', views.emotion_analysis, name="emotion_anaylsis"),
+    path(r'^type/$', views.emotion_analysis_type, name="emotion_analysis_type"),
+    path(r'^import/$', views.emotion_analysis_import, name="emotion_analysis_import"),
 ]
--- sentiment/urls.py
+++ sentiment/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 app_name = 'sentiment'
 
 urlpatterns = [
-    url(r'^$', views.sentiment_analysis, name="sentiment_anaylsis"),
-    url(r'^type/$', views.sentiment_analysis_type, name="sentiment_analysis_type"),
-    url(r'^import/$', views.sentiment_analysis_import, name="sentiment_analysis_import"),
+    path('', views.sentiment_analysis, name="sentiment_anaylsis"),
+    path('/type', views.sentiment_analysis_type, name="sentiment_analysis_type"),
+    path('/import', views.sentiment_analysis_import, name="sentiment_analysis_import"),
 ]
